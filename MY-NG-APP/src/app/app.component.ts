b106f0ddb2d58892a64004dd5790169de92d252d
SoniRe <helloadityayo@gmail.com>
2024-03-06 21:35:44 +0530

    Dashboard Completed

--- MY-NG-APP/src/app/app.component.ts
+++ MY-NG-APP/src/app/app.component.ts
@@ -3,11 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { CommonModule } from '@angular/common';
 import { User } from './models/user';
+import { JokeComponent } from './components/joke/joke.component';
+import { JokeService } from './services/joke.service';
+import { AComponent } from './components/a/a.component';
+import { B1Component } from './components/b1/b1.component';
+import { C2Component } from './components/c2/c2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserProfileComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    UserProfileComponent,
+    CommonModule,
+    JokeComponent,
+    AComponent,
+    B1Component,
+    C2Component,
+  ],
   templateUrl: './app.component.html',
   // template: `<h1>Hello bro angular</h1>`, // Custom Inline HTML
   styleUrl: './app.component.css',
---
+++ MY-NG-APP/src/app/services/joke.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JokeService {
+  constructor(private http: HttpClient) {}
+
+  getJoke() {
+    return this.http.get(
+      'https://api.chucknorris.io/jokes/random?category=dev'
+    );
+  }
+}
